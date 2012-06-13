3ca358e8d390d69fe6f476e2f417e4637e7b9882
Leonardo Casalli <chacalo79@hotmail.com>
2012-06-13 12:55:34 -0400

    Edicion miercoles 13 junio 12:55

---
+++ app/controllers/Login.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+import play.data.*;
+import java.util.*;
+import views.html.*;
+
+import models.*;
+
+public class Login extends Controller {
+    
+    /**
+     * Define una forma de envolver la clase de usuario.
+     */ 
+    final static Form<Usuario> loginForm = form(Usuario.class);
+  
+    /**
+     * Mostrar un formulario en blanco.
+     */ 
+    public static Result login() {
+        return ok(login.render(loginForm));
+    }
+  
+    /**
+     * Mostrar un formulario pre-llenado con una cuenta existente.
+     */
+    public static Result edit() {
+    	loginForm.bindFromRequest();
+        Usuario datos = loginForm.get();
+        datos.getCorreo();
+        datos.getPass();
+        return ok("Weeeena!");
+    }
+  
+  
+  
+}
