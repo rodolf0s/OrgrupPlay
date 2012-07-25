d493c5dee69efeedeef3235e64e9e197a432194c
Rodolfo Santander <rodolfo.santander06@gmail.com>
2012-07-25 16:18:58 -0400

    Mi cuenta guarda la imagen de perfil

--- app/controllers/Cuenta.java
+++ app/controllers/Cuenta.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -16,13 +17,10 @@ import views.html.agenda.miCuenta;
 
 public class Cuenta extends Controller {
 	
-public static class ConfiguraCuenta {
-        
+	public static class ConfiguraCuenta {        
         public String nombre;
         public String pass;
-        public String passNew;
-//        public String imagen;
-           
+        public String passNew;           
     }
 
 	private static ConexionJDBC conexion = ConexionJDBC.getInstancia();
@@ -37,10 +35,16 @@ public static class ConfiguraCuenta {
 	}
 	
 	// Actualiza los datos del usuario
-	public static Result configuraCuenta() throws SQLException {
+	public static Result configuraCuenta() throws SQLException, IOException {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		String sql = "";
+		String nombre = "";
+		String pass = "";
+		String passNew = "";
+		String extension = "";
+		Boolean imagen = false;
+		String filename = "";
 		
 		if(!verificaSession()) {
 			return redirect(routes.Application.index());
@@ -51,84 +55,108 @@ public static class ConfiguraCuenta {
 	            return badRequest(login.render(""));
 			} else {
 				
+				ConfiguraCuenta user = formCuenta.get();
+				nombre = user.nombre;
+				pass = user.pass;
+				passNew = user.passNew;				
+				
 				MultipartFormData body = request().body().asMultipartFormData();
 				FilePart picture = body.getFile("imagen");
 				
-				/*
-				 *  Recibe un archivo para subirlo al sv
-				 *  Aun no esta completo
-				 *  el archivo se sube a /tmp/
-				 */
-				if (picture != null) {
-				    String fileName = picture.getFilename();
+				// Verifica si el usuario cambia la imagen de su cuenta
+				if (picture != null) {					
 				    String contentType = picture.getContentType(); 
 				    File file = picture.getFile();
-				    String path = file.getAbsolutePath();
-				    //org.apache.commons.io.FileUtils.copyFile(path, /home/chino)
-				    return ok(path);
-//				    return ok(prueba.render(fileName, contentType));
+				    
+				    // verifica el tipo de imagen de la imagen a subir.
+				    if(contentType.equals("image/png")) {
+				    	extension = ".png";
+				    }
+				    else if(contentType.equals("image/jpeg")) {
+				    	extension = ".jpg";
+				    }
+				    else if(contentType.equals("image/gif")) {
+				    	extension = ".gif";
+				    }
+				    
+				    imagen = true;				    
+				    String path = "./public/images/usuarios/";
+				    filename = path + session("email") + extension;
+				    org.apache.commons.io.FileUtils.copyFile(file, new File(filename));
 				} else {
-//				    flash("error", "Missing file");
-					return ok("NO");
-//				    return ok(prueba.render("malo", "maloo"));    
+					imagen = false;
 				}
 				
+				try {
+					Connection con = conexion.abre();
+									
+					if(pass.equals("") && passNew.equals("") && imagen == false) {
+						sql = "UPDATE usuario SET nombre = '"+nombre+"' WHERE correo = '"+session("email")+"'";
+						st = con.prepareStatement(sql);
+						st.executeUpdate();
+						session("nombre", nombre);
+						return redirect (routes.Cuenta.miCuenta());
+					} 
+					else if(pass.equals("") && passNew.equals("") && imagen == true) {
+						sql = "UPDATE usuario SET nombre = '"+nombre+"', imagen = '"+filename+"' WHERE correo = '"+session("email")+"'";
+						st = con.prepareStatement(sql);
+						st.executeUpdate();
+						session("nombre", nombre);
+						return redirect (routes.Cuenta.miCuenta());
+					}
+					else if(imagen == false) {
+						sql = "SELECT password FROM usuario WHERE correo = '"+session("email")+"'";
+						st = con.prepareStatement(sql);
+						rs = st.executeQuery();
+						
+						rs.next();
+						if(pass.equals(rs.getString("password"))) {
+							if(passNew != "") {
+								sql = "UPDATE usuario SET nombre = '"+nombre+"', password = '"+passNew+"' WHERE correo = '"+session("email")+"'";
+								st = con.prepareStatement(sql);
+								st.executeUpdate();
+								session("nombre", nombre);
+								return redirect (routes.Cuenta.miCuenta());
+							} else {
+								return ok(miCuenta.render(session("nombre"), session("email"), "", "Ingrese una nueva Contraseña"));
+							}								
+						} else {
+							return ok(miCuenta.render(session("nombre"), session("email"), "La contraseña es incorrecta", ""));
+						}
+					}
+					else {
+ 						
+						sql = "SELECT password FROM usuario WHERE correo = '"+session("email")+"'";
+						st = con.prepareStatement(sql);
+						rs = st.executeQuery();
+						
+						rs.next();
+						if(pass.equals(rs.getString("password"))) {
+							if(passNew != "") {
+								sql = "UPDATE usuario SET nombre = '"+nombre+"', password = '"+passNew+"', imagen = '"+filename+"' WHERE correo = '"+session("email")+"'";
+								st = con.prepareStatement(sql);
+								st.executeUpdate();
+								session("nombre", nombre);
+								return redirect (routes.Cuenta.miCuenta());
+							} else {
+								return ok(miCuenta.render(session("nombre"), session("email"), "", "Ingrese una nueva Contraseña"));
+							}								
+						} else {
+							return ok(miCuenta.render(session("nombre"), session("email"), "La contraseña es incorrecta", ""));
+						}
+					}
+						
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+				finally{
+					conexion.cierra();
+				}
 				
-				
-//				ConfiguraCuenta user = formCuenta.get();
-//				String nombre = user.nombre;
-//				String pass = user.pass;
-//				String passNew = user.passNew;
-//			
-//				try {
-//					Connection con = conexion.abre();
-//					
-//				
-//					if(pass == "" && passNew == "") {
-//						sql = "UPDATE usuario SET nombre = '"+nombre+"' WHERE correo = '"+session("email")+"'";
-//						st = con.prepareStatement(sql);
-//						st.executeUpdate();
-//						
-//						
-////						st = con.prepareStatement(sql);
-////						st.execute(sql);
-//						session("nombre", nombre);
-//						return redirect (routes.Cuenta.miCuenta());
-//					} else {
-// 						
-//						sql = "SELECT password FROM usuario WHERE correo = '"+session("email")+"'";
-//						st = con.prepareStatement(sql);
-//						rs = st.executeQuery();
-//						
-//						rs.next();
-//						if(pass.equals(rs.getString("password"))) {
-//							if(passNew != "") {
-//								sql = "UPDATE usuario SET nombre = '"+nombre+"', password = '"+passNew+"' WHERE correo = '"+session("email")+"'";
-//								st = con.prepareStatement(sql);
-//								st.executeUpdate();
-//								
-////								st = con.prepareStatement(sql);
-////								st.execute(sql);
-//								session("nombre", nombre);
-//								return redirect (routes.Cuenta.miCuenta());
-//							} else {
-//								return ok(miCuenta.render(session("nombre"), session("email"), "", "Ingrese una nueva Contraseña"));
-//							}								
-//						} else {
-//							return ok(miCuenta.render(session("nombre"), session("email"), "La contraseña es incorrecta", ""));
-//						}
-//					}
-//						
-//				}catch(Exception e){
-//					e.printStackTrace();
-//				}
-//				finally{
-//					conexion.cierra();
-//				}
-//				
 			}
-//			return ok();
-//			return ok(prueba.render(sql, "", "", ""));
+			return ok();
+			
+//			return ok(nombre);
 //			return ok(miCuenta.render(session("nombre"), session("email")));
 		}
 		
