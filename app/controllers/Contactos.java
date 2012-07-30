f7d4d13d97a4a7d2ace286aae385a400b4048525
Daniel Rivera <danieldk8@gmail.com>
2012-07-30 15:45:12 -0400

    contactos

--- app/controllers/Contactos.java
+++ app/controllers/Contactos.java
@@ -1,6 +1,9 @@
 package controllers;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 import models.Contacto;
 
@@ -22,15 +25,44 @@ public class Contactos extends Controller {
 		String usuario1;
 		String usuario2;
 		ResultSet rs = null;
-		String sql;
+		String sql = "";
+		Connection con = null;
+
+		//Creo un array
+		ArrayList String = new ArrayList<String>();
 		
 		Form<Contacto> formBuscaContactos = form(Contacto.class).bindFromRequest();
 		
 		if(formBuscaContactos.hasErrors()){
 			return badRequest(contactos.render(session("nombre")));
+			
 		} else{
+			
 			Contacto amigos = formBuscaContactos.get();
 			usuario2 = amigos.usuario_correo2;
+			
+			try{
+				con = conexion.abre();
+				
+				sql = "SELECT * FROM usuario WHERE usuario_correo2 = '"+usuario2+"'";
+				PreparedStatement st = con.prepareStatement(sql);
+				rs = st.executeQuery();
+				
+				while(rs.next()){
+					String l = rs.getString("correo");
+					l +=rs.getString("nombre");
+				}
+				
+				return ok(contactos.render("l"));
+				
+//				if(rs.getRow() >= 1){
+//					rs.getString("nombre");
+//					return ok(contactos.render(l));
+//				}
+				
+			}catch(Exception e){ 
+				e.printStackTrace();
+			}
 			return ok();
 		}
 	}
