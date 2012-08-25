fa666d55e8bed953df81176ced4620d6aad07e28
Rodolfo Santander <rodolfo.santander06@gmail.com>
2012-08-25 17:03:30 -0400

    Arreglado los comentarios del modelo.

--- app/models/Usuario.java
+++ app/models/Usuario.java
@@ -11,9 +11,7 @@ import com.avaje.ebean.Ebean;
 import com.avaje.ebean.Expr;
 
 import play.data.format.Formats;
-import play.data.validation.Constraints;
 import play.db.ebean.Model;
-import play.db.ebean.Model.Finder;
 
 @Entity
 @Table(name="usuario")
@@ -54,12 +52,14 @@ public class Usuario extends Model {
 	// Consultas
 	
 	public static Finder<String,Usuario> find = new Finder<String,Usuario>(String.class, Usuario.class);
-
 	
 	/**
-	 *	Auntentifica el usuario	
+	 * Verifica si el usuario esta registrado en la BD.
+	 * 
+	 * @param correo 
+	 * @param password
+	 * @return un objeto con los datos si se encuentra en la BD.
 	 */
-
 	public static Usuario authenticate(String correo, String password) {
         return find.where()
             .eq("correo", correo)
@@ -67,10 +67,12 @@ public class Usuario extends Model {
             .findUnique();
     }
 
-    /**
-     * Consulta si existe un usuario determinado
-     */
-	
+	/**
+	 * Verifica si el usuario que se va a registrar existe en la BD.
+	 * 
+	 * @param correo
+	 * @return si es mayor que cero, el usuario ya esta registrado.
+	 */
     public static boolean esMiembro(String correo) {
         return find.where()
             .eq("correo", correo)
@@ -78,9 +80,14 @@ public class Usuario extends Model {
     }
     
     /**
-     * Consulta si existe el id_verificador
+     * Comprueba que el id_verificador que se le asigna a una cuenta
+     * no exista en la BD, para asignarselo a otro usuario.
+     * 
+     * @param idVerificador
+     * 			Parametro que es generado desde Application.java
+     * @return si es mayor que cero el id_verificador ya se encuentra
+     * en la BD, por lo tanto se genera otro en el Controlador.
      */
-    
     public static boolean estaIdVerificador(Integer idVerificador) {
     	return find.where()
     		.eq("id_verificador", idVerificador)
@@ -88,33 +95,42 @@ public class Usuario extends Model {
     }
     
     /**
-     * comprueba la cuenta, viendo si el id llegado por url
-     * esta en la BD y si es unico
+     * Comprueba la cuenta para activarla, verificando que el id
+     * llegado en la url este en la BD y si es unico.
+     * 
+     * @param correo
+     * @param idVerificador
+     * 			llega desde la url enviada al correo del usuario registrado
+     * @return si es unico retorna un 1.
      */
-
     public static boolean verificaCuenta(String correo, Integer idVerificador) {
     	return find.where()
     		.eq("correo", correo)
     		.eq("id_verificador", idVerificador)
     		.findRowCount() == 1;
     }
-    
+
     /**
-     * acutializa el estado del usuario, una vez activada
+     * Actualiza el estado de la cuenta del usuario despues de
+     * haber activado la cuenta.
+     * 
+     * @param correo
+     * @param idVerificador
      */
- 
-    
     public static void actualizaEstado(String correo, Integer idVerificador) {
     	Ebean.createSqlUpdate(
     			"update usuario set estado = 'Activada' where " +
     			"correo = '"+correo+"' and id_verificador = '"+idVerificador+"'"
     			).execute();
     }
-    
+
     /**
-     * verifica que la cuenta este activada
+     * Verifica que la cuenta este activada, sino al usuario no
+     * se le dejara ingresar al sistema.
+     * 
+     * @param correo
+     * @return si esta "Activada" retorna un 1.
      */
-    
     public static boolean cuentaActivada(String correo) {
     	return find.where()
     			.eq("correo", correo)
@@ -123,9 +139,12 @@ public class Usuario extends Model {
     }
     
     /**
-     * Obtiene la password de la BD de un determiando usuario
+     * Obtiene la password de un determinado usuario.
+     * 
+     * @param correo
+     * @return si la password se encuentra en la Bd la retorna,
+     * en caso contrario retorna vacio ("").
      */
-    
     public static String getPassword(String correo) {
     	Usuario usuario = Ebean.find(Usuario.class)  
     	        .select("password")  
@@ -140,21 +159,29 @@ public class Usuario extends Model {
     	} catch(Exception e) {}
 		return "";
     }
-    
+
     /**
-     * Actualiza el nombre de usuario
+     * Actualiza el nombre del usuario.
+     * 
+     * @param nombre
+     * 		Nuevo nombre del usuario.
+     * @param correo
      */
-    
     public void setNombre(String nombre, String correo) {
         Usuario usuario = find.ref(correo);
         usuario.nombre = nombre;
         usuario.update();    
     }
-    
+
     /**
-     * Actualiza el nombre y la imagen del usuario
+     * Actualiza el nombre y la imagen del usuario.
+     * 
+     * @param correo
+     * @param nombre
+     * 		Nuevo nombre.
+     * @param imagen
+     * 		Nueva imagen.
      */
-    
     public void setImagen(String correo, String nombre, String imagen) {
         Usuario usuario = find.ref(correo);
         usuario.nombre = nombre;
@@ -163,28 +190,40 @@ public class Usuario extends Model {
     }
     
     /**
-     * Actualiza la contraseña del usuario
+     * Actualiza la password del usuario.
+     * 
+     * @param correo
+     * @param password
+     * 		Nueva password.
      */
-    
     public void setPassword(String correo, String password) {
     	Usuario usuario = find.ref(correo);
     	usuario.password = password;
     	usuario.update();
     }
-    
+
     /**
-     * Obtiene una lista de los usuarios, para agregar a sus contactos
+     * Obtiene una lista de los usuario donde sea igual
+     * o parecido al nombre o correo.
+     * 
+     * @param nombre
+     * 		busca usuarios por el nombre.
+     * @return una lista de todos los usuarios encontrados con el nombre
      */
     public static List<Usuario> listaUsuarios(String nombre){
     	return find.where()
-    			.or(Expr.like("nombre", "%"+nombre+"%"),  Expr.like("correo", "%"+nombre+"%")).findList();
+    			.or(Expr.like("nombre", "%"+nombre+"%"),  Expr.like("correo", "%"+nombre+"%"))
+    			.findList();
   	
     }
     
     /**
-     * Muestra nombre de usuario atraves del email
+     * Obtiene el nombre de usuario.
+     * 
+     * @param email
+     * 		Busca el nombre por el correo del usuario.
+     * @return el nombre de usuario.
      */
-    
 	public static String muestraNombre(String email){
 	    Usuario usuario = Ebean.find(Usuario.class)
 	    		.select("nombre")
@@ -193,11 +232,14 @@ public class Usuario extends Model {
 	    		.findUnique();
 	    return usuario.nombre;
 	}
-	
+
 	/**
-	 * Obtiene imagen de usuario a traves del correo
+	 * Obtiene la imagen del usuario.
+	 * 
+	 * @param email
+	 * 		Busca una imagen por el correo del usuario.
+	 * @return la imagen del usuario.
 	 */
-	
 	public static String muestraImagen(String email) {
 		Usuario usuario = Ebean.find(Usuario.class)
 				.select("imagen")
