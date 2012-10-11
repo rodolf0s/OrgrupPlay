906ff24995d70eb27568b493c747af09d280a70c
Leonardo Casalli <chacalo79@hotmail.com>
2012-10-11 17:41:13 -0400

    Almacenar en vectores bloques libres con sus fechas y horas correspondientes

--- app/controllers/Reuniones.java
+++ app/controllers/Reuniones.java
@@ -17,9 +17,11 @@ public class Reuniones extends Controller {
 
 	public static Result generarReunion(String correo) {
 		Integer numero= 0;
-		Integer [][] bloques = {};
-		Date [] dias = {};
-		Date [] horas = {};
+//		Integer [] bloque = new Integer [50];
+		Integer [] bloque = null;
+		Date [] dias = null;
+		Date [] horas = null;
+		Integer posiciones = null;
 		
  		Form<Reunion> formReunion = form(Reunion.class).bindFromRequest();
 		
@@ -40,16 +42,34 @@ public class Reuniones extends Controller {
 			Calendar horaFinCalendar = new GregorianCalendar(); 
 			fechaInicioCalendar.setTime(fechaInicio);
 			fechaFinCalendar.setTime(fechaFin);
+			horaInicioCalendar.setTime(horaInicio);
+			horaFinCalendar.setTime(horaFin);
 			
 			//aumentamos en 1 la fecha de fin para la busqueda porque before no cuenta el igual
 			fechaFinCalendar.add(fechaFinCalendar.DAY_OF_MONTH, +1);
 			
+			//Calcular los dias que se revizaran
+			Integer diferenciaDias =  fechaFinCalendar.get(Calendar.DAY_OF_YEAR) - fechaInicioCalendar.get(Calendar.DAY_OF_YEAR);
+			
+			//Calcular las horas que se revizaran
+			Integer diferenciaHoras = horaFinCalendar.get(Calendar.HOUR_OF_DAY) - horaInicioCalendar.get(Calendar.HOUR_OF_DAY);
+			diferenciaHoras = diferenciaHoras + 1;
+			
+			//Calcular la catidad de posiciones del vector
+			posiciones = diferenciaDias * diferenciaHoras;
+			
+			//Inicializar los vectores
+			bloque = new Integer[posiciones];
+			dias = new Date [posiciones];
+			horas = new Date [posiciones];
+			
+			//puntero del vector
 			Integer a = 0;
 			
 			//Revisamos dias con bloques libres del administrador
 			while(fechaInicioCalendar.before(fechaFinCalendar)) {
 				
-				Integer b = 0;				
+				//reiniciar el valor de las horas			
 				horaInicioCalendar.setTime(horaInicio);
 				horaFinCalendar.setTime(horaFin);
 				
@@ -60,22 +80,25 @@ public class Reuniones extends Controller {
 					Integer estado = null;
 					Date fechaInicio1 = fechaInicioCalendar.getTime();
 					Date horaInicio1 = horaInicioCalendar.getTime();
+				
+					//guardar la fecha del bloque
+					dias[a] = fechaInicio1;
+					horas[a] = horaInicio1; 
 					
-					estado = Tarea.buscarTarea(fechaInicio1, horaInicio1, correo);
 					//Indica si el bloque esta libre o no
+					estado = Tarea.buscarTarea(fechaInicio1, horaInicio1, correo);
 					if (estado > 0){
-						numero = numero + 1;
+						bloque [a] = 1;
 					}else{
-
+						bloque [a] = 0;
 					}
 
 					horaInicioCalendar.add(horaInicioCalendar.HOUR, +1);
-					b = b+1;
+					a = a+1;
 				}
 				fechaInicioCalendar.add(fechaInicioCalendar.DAY_OF_MONTH, +1);
-				a = a+1;
 			}
 	}
-		return ok(mensajeReunion.render(session("email"), numero, correo));
+		return ok(mensajeReunion.render(session("email"), numero, correo, bloque, posiciones, dias, horas));
 	}
 }
