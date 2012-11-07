73619fae2d05abbafea1f42f4506d115cabd0494
Rodolfo Santander <rodolfo.santander06@gmail.com>
2012-11-07 18:51:34 -0300

    Mostrando datos de la reunion

--- app/controllers/Algoritmo.java
+++ app/controllers/Algoritmo.java
@@ -1,13 +1,31 @@
 package controllers;
 
+import models.Integrante;
+import models.Reunion;
+import models.Tarea;
+import models.Usuario;
+
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
-
-import views.html.grupo.*;
 
 public class Algoritmo extends Controller {
 
     public static Result index() {
-        return ok();
+        return ok(views.html.grupo.reunion.render());
+    }
+
+    public static Result calcula() {
+        Form<Reunion> formReunion = form(Reunion.class).bindFromRequest();
+        Reunion reunion = formReunion.get();
+
+        return ok(views.html.grupo.muestra.render(
+            reunion.nombre,
+            reunion.descripcion,
+            reunion.fecha_inicio.toString(),
+            reunion.fecha_fin.toString(),
+            reunion.hora_inicio.toString(),
+            reunion.hora_fin.toString(),
+            reunion.duracion.toString()));
     }
 }
